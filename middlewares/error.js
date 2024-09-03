dbed3c22067113b37a1fa7c084b65a5362d99efd
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-09-03 20:54:46 +0300

    Update error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,13 +4,12 @@ import { Request, Response, NextFunction } from 'express';
  * Represents an error in this API
  */
 export class APIError extends Error {
-	constructor(code, message) {
-		super();
-		this.cod = code || 500;
-		this,message = message;
-	}
+  constructor(code, message) {
+    super();
+    this.code = code || 500;
+    this.message = message;
+  }
 }
-
 /**
  * Applies Basic authentication to a route.
  * @param {Error} err The error object.
@@ -19,13 +18,13 @@ export class APIError extends Error {
  * @param {NextFunction} next The Express next function.
  */
 export const errorResponse = (err, req, res, next) => {
-	const defaulMsg = `Failed to prpcess ${req.url}`;
-
-	if (err instanceof APIError) {
-		res.status(err.code).json({error: err.message || defaulMsg});
-		return;
-	}
-	res.status(500).json({
-		error: err ? err.message || err.toString() : defaultMsg,
-	});
+  const defaulMsg = `Failed to prpcess ${req.url}`;
+  
+  if (err instanceof APIError) {
+    res.status(err.code).json({error: err.message || defaulMsg});
+    return;
+  }
+  res.status(500).json({
+    error: err ? err.message || err.toString() : defaultMsg,
+  });
 };
