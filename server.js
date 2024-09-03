a0f38fa451ff0b9b171806e75cd8eabc5acc0bc2
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-09-03 21:28:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import startServer from './libs/boot';
 import injectRoutes from'./routes';
+import injectMiddlewares from './libs/middlewares';
 
 const server = express();
 
+injectMiddlewares(server);
 injectRoutes(server);
 startServer(server);
 
