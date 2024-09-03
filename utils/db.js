337c4c32d75dedb224c197f12b9af1e9b29ac075
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-09-03 20:39:41 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -21,43 +21,43 @@ class DBclient {
     this.client.connect();
   }
 
-	/**
-	 * Checks if this client's connection to the MongoDB server is active.
-	 * @returns {boolean}
-	 */
-	isAlive() {
+  /**
+   * Checks if this client's connection to the MongoDB server is active
+   * @returns {boolean}
+   */
+  isAlive() {
     return this.client.isConnected();
-	}
+  }
 
-	/**
-	 * Retrieves the number of users in the database.
-	 * @returns {Promise<Number>}
-	 */
-	async nbUsers() {
+  /**
+   * Retrieves the number of users in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbUsers() {
     return this.client.db().collection('users').countDocuments();
-	}
-
-	/**
-	 * Retrieves the number of files in the database.
-	 * @returns {Promise<Number>}
-	 */
-	async nbFiles() {
+  }
+  
+  /**
+   * Retrieves the number of files in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbFiles() {
     return this.client.db().collection('files').countDocuments();
   }
 
-	/**
-	 * Retrieves a reference to the `users` collection.
-	 * @returns {Promise<Collection>}
-	 */
-	async usersCollection() {
+  /**
+   * Retrieves a reference to the `users` collection.
+   * @returns {Promise<Collection>}
+   */
+  async usersCollection() {
     return this.client.db().collection('users');
   }
-
-	/**
-	 * Retrieves a reference to the `files` collection.
-	 * @returns {promise<Collection>}
-	 */
-	async filesCollection() {
+  
+  /**
+   * Retrieves a reference to the `files` collection.
+   * @returns {promise<Collection>}
+   */
+  async filesCollection() {
     return this.client.db().collection('files');
   }
 }
