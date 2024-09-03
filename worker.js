28c7347b97cc49c81a24bf25024fcc24ef840586
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-09-03 21:25:49 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-named-as-default */
 import { writeFile } from 'fs';
 import { promisify } from 'util';
 import Queue from 'bull/lib/queue';
@@ -17,64 +18,64 @@ const userQueue = new Queue('email sending');
  * @returns {Promise<void>}
  */
 const generateThumbnail = async (filePath, size) => {
-	const buffer = await imgThumbnail(filePath, { width: size });
-	console.log(`Generating file: ${filePath}, size: ${size}`);
-	return writeFileAsync(`${filePath}_${size}`, buffer);
+  const buffer = await imgThumbnail(filePath, { width: size });
+  console.log(`Generating file: ${filePath}, size: ${size}`);
+  return writeFileAsync(`${filePath}_${size}`, buffer);
 };
 
 fileQueue.process(async (job, done) => {
-	const fileId = job.data.fileId || null;
-	const userId = job.data.userId || null;
-
-	if (!fileId) {
-		throw new Error('Missing fileId');
-	}
-	if (!userId) {
-		throw new Error('Missing userId');
-	}
-	console.log('Processing', job.data.name || '');
-	const file = await (await dbClient.filesCollection())
-	  .findOne({
-		  _id: new mongoDBCore.BSON.ObjectId(fileId),
-		  userId: new mongoDBCore.BSON.ObjectId(userId),
-	  });
-	if (!file) {
-		throw new Error('File not found');
-	}
-	const sizes = [ 500, 250, 100];
-	Promise.all(sizes.map((size) => generateThumbnail(file.localPath, size)))
-	  .then(() => {
-		  done();
-	  });
+  const fileId = job.data.fileId || null;
+  const userId = job.data.userId || null;
+  
+  if (!fileId) {
+    throw new Error('Missing fileId');
+  }
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+  console.log('Processing', job.data.name || '');
+  const file = await (await dbClient.filesCollection())
+    .findOne({
+      _id: new mongoDBCore.BSON.ObjectId(fileId),
+      userId: new mongoDBCore.BSON.ObjectId(userId),
+    });
+  if (!file) {
+    throw new Error('File not found');
+  }
+  const sizes = [ 500, 250, 100];
+  Promise.all(sizes.map((size) => generateThumbnail(file.localPath, size)))
+    .then(() => {
+      done();
+    });
 });
 
 userQueue.process(async (job, done) => {
-	const userId = job.data.userId || null;
-
-	if (!userId) {
-		throw new Error('Missing userId');
-	}
-	const user = await (await dbClient.usersCollection())
-	  .fincOne({ _id: new mongoDBCore.BSON.ObjectId(userId) });
-	if (!user) {
-		throw new Error('User not found');
-	}
-	console.log(`Welcome ${user.email}!`);
-	try {
-		const mailSubject = 'Welcome to ALX-Files_Manager by lioneljuru';
-		const mailContent = [
-			'<div>',
-			'<h3>Hello {{user.name}},</h3>'
-			'Wecone to <a href="https://github.com/lioneljuru/alx-files_manager">',
-			'ALX-Files_Manager</a>, ',
-			'a simple file management API built with Node.js by ',
-			'<a href="https://github.com/lioneljuru">JURU Jean Lionel</a>
-			'We hope it meest your needs.',
-			'</div>',
-		].join('');
-		Mailer.sendMail(Mailer.buildMessage(user.email, mailSubject, mailContent));
-		done();
-	} catch (err) {
-		done(err);
-	}
+  const userId = job.data.userId || null;
+  
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+  const user = await (await dbClient.usersCollection())
+    .fincOne({ _id: new mongoDBCore.BSON.ObjectId(userId) });
+  if (!user) {
+    throw new Error('User not found');
+  }
+  console.log(`Welcome ${user.email}!`);
+  try {
+    const mailSubject = 'Welcome to ALX-Files_Manager by lioneljuru';
+    const mailContent = [
+      '<div>',
+      '<h3>Hello {{user.name}},</h3>'
+      'Wecone to <a href="https://github.com/lioneljuru/alx-files_manager">',
+      'ALX-Files_Manager</a>, ',
+      'a simple file management API built with Node.js by ',
+      '<a href="https://github.com/lioneljuru">JURU Jean Lionel</a>
+      'We hope it meest your needs.',
+      '</div>',
+    ].join('');
+    Mailer.sendMail(Mailer.buildMessage(user.email, mailSubject, mailContent));
+    done();
+  } catch (err) {
+    done(err);
+  }
 });
