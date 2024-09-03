be18f4adb090070189431b35288a0c4ba80c7624
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-09-03 20:33:50 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -10,23 +10,23 @@ class DBclient {
 	/**
 	 * Creates a new DBClient inantce.
 	 */
-	constructor() {
-		envLoader();
-		const host = process.env.DB_HOST || 'localhost';
-		const port = process.env.DB_PORT || 27017;
-		const database = process.env.DB_DATABASE || 'files_manager';
-		const dbURL = `mongodb://${host}:${port}/${database}`;
+  constructor() {
+    envLoader();
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
 
-		this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
-		this.client.connect();
-	}
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
 
 	/**
 	 * Checks if this client's connection to the MongoDB server is active.
 	 * @returns {boolean}
 	 */
 	isAlive() {
-		return this.client.isConnected();
+    return this.client.isConnected();
 	}
 
 	/**
@@ -34,7 +34,7 @@ class DBclient {
 	 * @returns {Promise<Number>}
 	 */
 	async nbUsers() {
-		return this.client.db().collection('users').countDocuments();
+    return this.client.db().collection('users').countDocuments();
 	}
 
 	/**
@@ -42,24 +42,24 @@ class DBclient {
 	 * @returns {Promise<Number>}
 	 */
 	async nbFiles() {
-		return this.client.db().collection('files').countDocuments();
-        }
+    return this.client.db().collection('files').countDocuments();
+  }
 
 	/**
 	 * Retrieves a reference to the `users` collection.
 	 * @returns {Promise<Collection>}
 	 */
 	async usersCollection() {
-		return this.client.db().collection('users');
-	}
+    return this.client.db().collection('users');
+  }
 
 	/**
 	 * Retrieves a reference to the `files` collection.
 	 * @returns {promise<Collection>}
 	 */
 	async filesCollection() {
-		return this.client.db().collection('files');
-        }
+    return this.client.db().collection('files');
+  }
 }
 
 export const dbClient = new DBClient();
